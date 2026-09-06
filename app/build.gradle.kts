9631e55585fbc997e8c83b9576ee713769580021
jungwon927 <73385488+jungwon927@users.noreply.github.com>
2026-09-06 15:47:49 +0900

    위젯 우측에 코식이 차 이미지 추가 (v1.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.jungwon927.kosiki"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
 
     buildTypes {
