b0f07e05e9b596260f453fe130773a75b8db09a9
jungwon927 <73385488+jungwon927@users.noreply.github.com>
2026-09-06 19:01:09 +0900

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.jungwon927.kosiki"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.2"
     }
 
     buildTypes {
